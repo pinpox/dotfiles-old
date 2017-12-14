c32b5a65b6ad92374fe743ffcf49ec289393e795
Pablo <pablo1@mailbox.org>
2017-12-14 15:12:33 +0100

    bootstrap sets shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
+
 GIT_REPO="https://gitlab.com/binaryplease/dotfiles.git"
-cd ~
-git clone --bare $GIT_REPO $HOME/.cfg
-function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+CONF_DIR_NAME=".dotfiles"
+
+function set_dotfiles() {
+	cd ~
+	git clone --bare $GIT_REPO $HOME/$CONF_DIR_NAME
+	function config {
+		/usr/bin/git --git-dir=$HOME/$CONF_DIR_NAME/ --work-tree=$HOME $@
+	}
+	echo "$CONF_DIR_NAME" >> .gitignore
+	mkdir -p .config-backup
+	config checkout
+	if [ $? = 0 ]; then
+		echo "Checked out config.";
+	else
+		echo "Backing up pre-existing dot files.";
+		config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+	fi;
+	config checkout
+	config config status.showUntrackedFiles no
 }
-echo ".cfg" >> .gitignore
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
-config checkout
-config config status.showUntrackedFiles no
+
+
+function set_zsh() {
+	read -p "Change shell to zsh? [Y/n]" -n 1 -r
+	echo
+	if [[ $REPLY =~ ^(y|Y| ) ]] || [[ -z $REPLY ]];
+	then
+		chsh -s /bin/zsh
+	fi
+}
+
+set_dotfiles
+set_zsh
