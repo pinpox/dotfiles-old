1b522c9d45bf67bd2cad890e08ba15b45d9430da
binaryplease <binaryplease@gmail.com>
2017-12-14 13:38:06 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,17 @@
-
 #!/bin/bash
-
-
 GIT_REPO="https://gitlab.com/binaryplease/dotfiles.git"
-
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-echo ".cfg" >> .gitignore
 git clone --bare $GIT_REPO $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+echo ".cfg" >> .gitignore
+mkdir -p .config-backup
 config checkout
-mkdir -p .config-backup && \
-	config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-	xargs -I{} mv {} .config-backup/{}
+if [ $? = 0 ]; then
+  echo "Checked out config2.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
 config checkout
-config config --local status.showUntrackedFiles no
+config config status.showUntrackedFiles no
