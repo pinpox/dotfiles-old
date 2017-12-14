1b076a14f8b999fd46ab34a02b0a1126d31ce91a
binaryplease <binaryplease@gmail.com>
2017-12-14 13:39:31 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 GIT_REPO="https://gitlab.com/binaryplease/dotfiles.git"
+cd ~
 git clone --bare $GIT_REPO $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
@@ -8,7 +9,7 @@ echo ".cfg" >> .gitignore
 mkdir -p .config-backup
 config checkout
 if [ $? = 0 ]; then
-  echo "Checked out config2.";
+  echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
