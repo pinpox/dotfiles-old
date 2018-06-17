0e296ca782468422fb72142ab6f19077877de978
Pablo <pablo1@mailbox.org>
2018-06-17 10:30:26 +0200

    removed no longer needed scripts

--- i3-ws-bachelor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-i3-msg "workspace 3; append_layout i3-workspace-ba.json"
-evince ~/Documents/FH/Bachelorarbeit/bachelorarbeit/Latex/src/Hauptdatei.pdf &
-termite -t "Bachelorarbeit Editor" --hold -d "Documents/FH/Bachelorarbeit/bachelorarbeit/Latex/src" &
-chromium &
