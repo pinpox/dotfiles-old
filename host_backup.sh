92ca77d979421dcdc4c2a71dec685b87a8d3ec40
Pablo <pablo1@mailbox.org>
2018-08-08 19:33:19 +0200

    backupscript makes checks before starting

--- host_backup.sh
+++ host_backup.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
+SERVER=sellerie
+REPOSITORY=binaryplease@$server:/mnt/backup/borgbackup/'{hostname}'
 
-REPOSITORY=binaryplease@sellerie:/mnt/backup/borgbackup/'{hostname}'
+# Check for root
+if [ "$EUID" -ne 0 ]
+then echo "Please run as root"
+	exit
+fi
+
+if ping -c1 -W1 sellerie; then
+	echo "Backup server $server is reachable, continuing"
+else
+	echo "Backup server $server is down, exiting."
+	exit
+fi
 
 # Backup list of installes packages
-echo "Creating package list"
+echo "Creating package list..."
 pacman -Qe > /home/binaryplease/installed_packages_list.txt
 
-# Backup all of /home and /var/www except a few
-# excluded directories
-echo "Creating backup"
+echo "Creating backup..."
 borg create -v --progress --stats \
---compression lz4 \
-$REPOSITORY::'{hostname}-{now:%Y-%m-%d}' \
-/etc \
-/home \
-/root \
---exclude '/home/*/.config/chromium' \
---exclude '/home/*/.local/share/Trash' \
---exclude '/home/*/.cache' \
---exclude '*.pyc'
+	--compression lz4 \
+	$REPOSITORY::'{hostname}-{now:%Y-%m-%d}' \
+	/etc \
+	/home \
+	/root \
+	--exclude '/home/*/.config/chromium' \
+	--exclude '/home/*/.local/share/Trash' \
+	--exclude '/home/*/.cache' \
+	--exclude '*.pyc'
 
-echo "deleting old backups"
+echo "Deleting old backups..."
 borg prune -v $REPOSITORY --prefix '{hostname}-' --keep-daily=3 --keep-weekly=2 --keep-monthly=6
+
+echo "Deleting package list..."
+rm -rf /home/binaryplease/installed_packages_list.txt
+
